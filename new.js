513d8824c421a56ce777da1f9332835b25d94131
aftab82389 <130346457+aftab82389@users.noreply.github.com>
2025-04-14 14:21:35 +0530

    Update new.js

--- new.js
+++ new.js
@@ -2,3 +2,6 @@ const newCont=document.querySelector('.newCont');
 newCont.addEventListener('click',()=>{
     window.location="new contact.html";
 })
+document.querySelector('.ri-arrow-left-line').addEventListener('click',()=>{
+  window.location="home.html"
+})
